2d17617dbfec0f6a784f2099eb7f3efb19f86aa7
Aitva <arod.louis@gmail.com>
2017-08-23 12:36:32 +0200

    Add template engine.

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+const fs = require('fs');
 const express = require('express');
 const mustache = require('mustache');
 
 let app = express();
 
-app.use(express.static("public"));
+app.get("/", function(req, resp) {
+    resp.render('index', {
+        name: 'Louis',
+        adjective: 'hungry'
+    });
+});
 
 app.get("/test", function(req, resp) {
     let str = mustache.render("Hello {{name}}!!! You are awesome!", {
@@ -11,6 +17,24 @@ app.get("/test", function(req, resp) {
     })
     resp.send(str)
 });
+
+app.engine("html", function(path, options, callback) {
+    fs.readFile(path, function(err, content) {
+        if (err) {
+            console.error("fail to open template:", err);
+            return callback(err);
+        }
+        let str = mustache.render(content.toString(), options);
+        return callback(null, str);
+    })
+});
+
+// specify the views directory
+app.set('views', './template');
+// register the template engine
+app.set('view engine', 'html');
+
+app.use(express.static("public"));
 
 app.listen(80, function() {
     console.log('Server listening on port 80...');
